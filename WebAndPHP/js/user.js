b07f82247661084dcddca9a61705b232968c6ef0
liujunfei <1115040996@qq.com>
2018-02-23 18:14:15 +0800

    添加 添加用户接口，对接添加用户接口。

--- WebAndPHP/js/user.js
+++ WebAndPHP/js/user.js
@@ -38,5 +38,35 @@ app.get('/user/deleteUser', function(req, res) {
 	});
 });
 
+app.get('/user/createUser', function(req, res) {
+	var user_name = req.query.name;
+	var user_age = req.query.age;
+	var user_address = req.query.address;
+	var sql = "insert into person(name,age,address) value("+"'"+user_name+"',"+user_age+",'"+user_address+"'"+")";
+	connection.query(sql,function(err,result){
+		if (err) {
+			res.send(err);
+		}else{
+			connection.query("SELECT id From Person WHERE NAME ="+"'"+user_name+"'",function(err,result){
+				if (err) {
+					res.send(JSON.stringify({"code":0,"msg":'插入数据成功!',userMsg:{
+						name:user_name,
+						age:user_age,
+						address:user_address,
+					}}));
+				}else{
+					res.send(JSON.stringify({"code":0,"msg":'插入数据成功!',userMsg:{
+						name:user_name,
+						age:user_age,
+						address:user_address,
+						id:result[0]["id"],
+					}}));
+				}
+			});
+			
+			
+		}
+	});
+});
 app.listen(8888);
 console.log('Listening on port 8888...');
